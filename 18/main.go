465067e5d5b69e52ff7fdc678d66b9cd04f34772
kenny <emrebadem_@hotmail.com>
2022-09-29 18:30:57 +0300

    [18] Add optimized solution

--- 18/main.go
+++ 18/main.go
@@ -1,26 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Printf("%v")
+	readFile, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatalf("failed to open file: %s", err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var tri [][]int
+	for fileScanner.Scan() {
+		scannedStrings := strings.Split(fileScanner.Text(), " ")
+		var arrToAppend []int
+		for _, strNum := range scannedStrings {
+
+			intVar, _ := strconv.Atoi(strNum)
+			arrToAppend = append(arrToAppend, intVar)
+
+		}
+		tri = append(tri, arrToAppend)
+	}
+	fmt.Printf("%d\n", findHighestChainSumInTri(tri))
+
 }
 
-func findHighestChainSumInTri(arr [][]int) {
-	for n := 0; n < len(arr)-2; n++ {
-		higestNum, pos := findHighestNumInArr(arr[n+1])
-		higestNum
-		maxValueOfCurrentLine := 0
-		arr[n]
+func max(x, y int) int {
+	if x > y {
+		return x
 	}
+	return y
 }
-func findHighestNumInArr(nums []int) (int, int) {
-	highestNum := 0
-	highestNumPos := 0
-	for pos, num := range nums {
-		if num > highestNum {
-			highestNum, highestNumPos = highestNum, pos
+
+func findHighestChainSumInTri(arr [][]int) int {
+	for i := len(arr) - 2; i >= 0; i-- {
+		for a := 0; a < len(arr[i]); a++ {
+			highest := max(arr[i+1][a], arr[i+1][a+1])
+			arr[i][a] = arr[i][a] + highest
 		}
+		fmt.Printf("[%v]\n", arr[i])
+
 	}
-	return highestNum, highestNumPos
+	return arr[0][0]
 }
